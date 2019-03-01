3386ecaffc95da6009942593ab08af317d0564ca
DevandScorp <artemda459@gmail.com>
2019-03-01 20:13:56 +0300

    Updates

---
+++ precache-manifest.bb9cb0bbd93c78caf9370926712c8e75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec5bc1b3148ca464a3a3e17af89c3ead",
+    "url": "/passport-token/static/media/github-brands.ec5bc1b3.svg"
+  },
+  {
+    "revision": "838a6dbac934b23a8909",
+    "url": "/passport-token/static/js/runtime~main.838a6dba.js"
+  },
+  {
+    "revision": "ac4f39983691d4afb5d4",
+    "url": "/passport-token/static/js/main.ac4f3998.chunk.js"
+  },
+  {
+    "revision": "a2b526ce81b6c8d3bc07",
+    "url": "/passport-token/static/js/2.a2b526ce.chunk.js"
+  },
+  {
+    "revision": "ac4f39983691d4afb5d4",
+    "url": "/passport-token/static/css/main.0f9b9fcf.chunk.css"
+  },
+  {
+    "revision": "a2b526ce81b6c8d3bc07",
+    "url": "/passport-token/static/css/2.23d471b5.chunk.css"
+  },
+  {
+    "revision": "ea8dc677e888d7a75540754d781450b7",
+    "url": "/passport-token/index.html"
+  }
+];
